cda6aea8867970a93981a52d66953e91a07bad48
Niels-tame <niels@tamethat.com>
2018-11-15 22:53:34 +0100

    version 0.1 component

--- example/src/App.js
+++ example/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 
 import CsvExportBtn from 'react-export-csv'
 
+const data = [
+  { firstName: 'Joe', lastName: 'Johnson' },
+  { firstName: 'Bob', lastName: 'Johnson' },
+]
+
 export default class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <CsvExportBtn text='download csv' />
+      <div style={{ padding: '20px' }}>
+        <CsvExportBtn
+          text="download csv"
+          filename="sample"
+          model={data}
+        />
       </div>
     )
   }
